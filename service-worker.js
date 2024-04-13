afa7bd158731680f5bd38d64bc36b054be5535d2
OND <149911739+ONDM@users.noreply.github.com>
2024-04-13 19:10:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,7 @@ self.addEventListener('install', (event) =>
       '/emat/font.woff2',
       '/emat/sw.js',
       '/emat/logo.png',
+      '/emat/pass.json',
       '/emat/folder/PV/PV1.jpg', '/emat/folder/PV/PV2.jpg', '/emat/folder/PV/PV3.jpg', '/emat/folder/PV/PV4.jpg',
       '/emat/folder/EMGV/EMGV1.jpg', '/emat/folder/EMGV/EMGV2.jpg', '/emat/folder/EMGV/EMGV3.jpg', '/emat/folder/EMGV/EMGV4.jpg',
       '/emat/folder/FSFZ/FSFZ1.jpg',
