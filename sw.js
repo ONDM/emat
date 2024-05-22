b829c54de5db20aca565fcb82424f25da6960b4d
OND <149911739+ONDM@users.noreply.github.com>
2024-05-22 19:06:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,32 @@
-// SERVICE-WORKER STATUS
-if ('serviceWorker' in navigator)
-{
-  navigator.serviceWorker.register('/emat/service-worker.js')
-  .then((registration) =>
+const cacheName = 'offline-link';
+const offlinePage = 'offline.html';
+const assets = [
+  '/emat/',
+  '/emat/style.css',
+  '/emat/script.js',
+  '/emat/favicon.png',
+  '/emat/font.woff2',
+];
+
+self.addEventListener('install', event =>
   {
-    console.log('Service Worker zaregistrován:', registration);
-  })
-  .catch((error) =>
+  event.waitUntil(
+    caches.open(cacheName).then(cache =>
+      {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event =>
   {
-    console.log('Chyba při registraci Service Worker:', error);
-  });
-}
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      {
+      return response || fetch(event.request);
+      }).catch(() =>
+      {
+      return caches.match(offlinePage);
+    })
+  );
+});
